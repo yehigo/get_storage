87c834a290926b0dc39ea67b52cd4dcd725f2f4e
Pintu Meena <pintulalmee@gmail.com>
2026-04-03 10:13:26 +0530

    removed dart:html dependecy with new package:web

--- example/linux/flutter/generated_plugins.cmake
+++ example/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  path_provider_linux
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
--- lib/src/storage/html.dart
+++ lib/src/storage/html.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
+import 'package:web/web.dart' show window, Storage;
 import '../value.dart';
 
 class StorageImpl {
   StorageImpl(this.fileName, [this.path]);
-  html.Storage get localStorage => html.window.localStorage;
+  Storage get localStorage_ => window.localStorage;
 
   final String? path;
   final String fileName;
@@ -15,16 +15,12 @@ class StorageImpl {
       ValueStorage<Map<String, dynamic>>(<String, dynamic>{});
 
   void clear() {
-    localStorage.remove(fileName);
-    subject.value.clear();
+    localStorage_.removeItem(fileName);
+    subject.value?.clear();
 
     subject
-      ..value.clear()
+      ..value?.clear()
       ..changeValue("", null);
-  }
-
-  Future<bool> _exists() async {
-    return localStorage.containsKey(fileName);
   }
 
   Future<void> flush() {
@@ -32,37 +28,37 @@ class StorageImpl {
   }
 
   T? read<T>(String key) {
-    return subject.value[key] as T?;
+    return subject.value?[key] as T?;
   }
 
   T getKeys<T>() {
-    return subject.value.keys as T;
+    return subject.value?.keys as T;
   }
 
   T getValues<T>() {
-    return subject.value.values as T;
+    return subject.value?.values as T;
   }
 
   Future<void> init([Map<String, dynamic>? initialData]) async {
-    subject.value = initialData ?? <String, dynamic>{};
-    if (await _exists()) {
-      await _readFromStorage();
-    } else {
-      await _writeToStorage(subject.value);
+    if (initialData != null) {
+      subject.value = initialData;
+      if (await _readFromStorage() == null) {
+        await _writeToStorage(subject.value);
+      }
     }
     return;
   }
 
   void remove(String key) {
     subject
-      ..value.remove(key)
+      ..value?.remove(key)
       ..changeValue(key, null);
     //  return _writeToStorage(subject.value);
   }
 
   void write(String key, dynamic value) {
     subject
-      ..value[key] = value
+      ..value?[key] = value
       ..changeValue(key, value);
     //return _writeToStorage(subject.value);
   }
@@ -71,29 +67,16 @@ class StorageImpl {
 
   // }
 
-  Future<void> _writeToStorage(Map<String, dynamic> data) async {
-    localStorage.update(fileName, (val) => json.encode(subject.value),
-        ifAbsent: () => json.encode(subject.value));
+  Future<void> _writeToStorage(Map<String, dynamic>? data) async {
+    if (data == null) return;
+    localStorage_.setItem(fileName, json.encode(data));
   }
 
-  Future<void> _readFromStorage() async {
-    final dataFromLocal = localStorage.entries.firstWhereOrNull(
-      (value) {
-        return value.key == fileName;
-      },
-    );
-    if (dataFromLocal != null) {
-      subject.value = json.decode(dataFromLocal.value) as Map<String, dynamic>;
-    } else {
-      await _writeToStorage(<String, dynamic>{});
-    }
-  }
-}
-
-extension FirstWhereExt<T> on Iterable<T> {
-  T? firstWhereOrNull(bool Function(T element) test) {
-    for (var element in this) {
-      if (test(element)) return element;
+  Future<Map<String, dynamic>?> _readFromStorage() async {
+    final dataFromLocal_ = localStorage_.getItem(fileName);
+    if (dataFromLocal_ != null) {
+      subject.value = json.decode(dataFromLocal_) as Map<String, dynamic>;
+      return subject.value;
     }
     return null;
   }
